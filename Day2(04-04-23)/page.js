4c3da5c66dea481b15150c44079d66d55d749897
Vijaykumar1154 <vijaykarthik1234.sg@gmail.com>
2023-04-09 21:56:39 +0530

    Session

---
+++ Day2(04-04-23)/page.js
@@ -0,0 +1,8 @@
+function showpopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+function hidepopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
