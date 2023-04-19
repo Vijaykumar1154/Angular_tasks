2da177de78e62bcd17fd746d40d0a51b04245c7f
Vijaykumar1154 <vijaykarthik1234.sg@gmail.com>
2023-04-19 15:40:38 +0530

    session

---
+++ Task9(19-04-23)/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { TestimonialComponent } from './pages/testimonial/testimonial.component';
+import { MenuBarComponent } from './common/menu-bar/menu-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { ContactBoxComponent } from './pages/contact/contact-box/contact-box.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceBoxComponent } from './pages/home/service-box/service-box.component';
+import { FeedbackBoxComponent } from './pages/testimonial/feedback-box/feedback-box.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    TestimonialComponent,
+    MenuBarComponent,
+    ContactBoxComponent,
+    ServiceBoxComponent,
+    FeedbackBoxComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
